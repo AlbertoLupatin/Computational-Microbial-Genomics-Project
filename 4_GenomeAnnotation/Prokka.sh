87ab518a265d4bdc745b273cf581548539aa3f54
Alberto Lupatin <55557905+AlbertoLupatin@users.noreply.github.com>
2024-09-13 09:50:37 +0200

    Update Prokka.sh
    Identifying and Labelling the relevant features in the genome such as coding sequences, genes coding for t-RNA, etc.

--- 4_GenomeAnnotation/Prokka.sh
+++ 4_GenomeAnnotation/Prokka.sh
@@ -1,4 +1,24 @@
-conda deactivate && conda activate prokka
- for i in $(ls *.fna); do
-      prokka --outdir ${i//.fna} --prefix ${i//.fna} $i --compliant;
+#!/bin/bash
+
+CONDA_BASE=$( conda info --base )
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+conda activate prokka
+
+prokka -h
+
+for f in /albertolupatin/Desktop/Genomics/Project/Samples/*
+do
+	mag=$( basename $f .fna)
+	mkdir -p prokka_output/${mag}
+	#echo $mag
+
+	prokka /Users/albertolupatin/Desktop/Genomics/Project/Samples/${mag}.fna \
+		--outdir prokka_output/${mag} \
+		--compliant \
+		--prefix ${mag} \
+		--force
+	
 done
+
+conda deactivate
