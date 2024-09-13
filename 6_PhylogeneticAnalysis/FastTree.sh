994770dcd986f4d8e2e46bbc95534e80a128c12d
Alberto Lupatin <55557905+AlbertoLupatin@users.noreply.github.com>
2024-09-13 09:57:16 +0200

    Create FastTree.sh

---
+++ 6_PhylogeneticAnalysis/FastTree.sh
@@ -0,0 +1,2 @@
+FastTreeMP -pseudo -spr 4 -mlacc 2 -slownni -fastest -no2nd -mlnni 4 -gtr -nt -out         \
+/Users/albertolupatin/Desktop/Genomics/Project/FastTree/core_gene_phylogeny.nwk /Users/albertolupatin/Desktop/Genomics/Project/Roary/roary_output/core_gene_alignment.aln
